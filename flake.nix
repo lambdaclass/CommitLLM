b6d1dbebb0bc5219e9167cf3fab654769c015bd8
MauroFab <maurotoscano2@gmail.com>
2026-03-23 18:43:29 -0300

    Fix missing SSL

--- flake.nix
+++ flake.nix
@@ -28,6 +28,9 @@
 
             pkgs.git
           ];
+
+          NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+          SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
         };
       });
 }
